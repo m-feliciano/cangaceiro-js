d6c2b276e79fc299bd6ec16bf14ababeb41f1035
Marcelo Feliciano <60995044+m-feliciano@users.noreply.github.com>
2022-01-19 22:31:40 -0300

    cap04 - fail fast

--- client/app/controllers/NegotiationController.js
+++ client/app/controllers/NegotiationController.js
@@ -1,27 +1,23 @@
 class NegotiationController {
+	constructor() {
+		let $ = document.querySelector.bind(document);
+		this._inputDate = $("#date");
+		this._inputQuantity = $("#quantity");
+		this._inputValue = $("#value");
+	}
 
-    constructor() {
-        let $ = document.querySelector.bind(document);
-        this._inputDate = $('#date');
-        this._inputQuantity = $('#quantity');
-        this._inputValue = $('#value');
-    }
+	add(event) {
+		event.preventDefault();
 
-    add(event) {
+        let date = DataConverter.toDate(this._inputDate.value);
         
-        event.preventDefault();
-
-        let date = new Date(...this._inputDate.value
-                .split('-')
-                .map((item, indice) => item - indice % 2)
-        );
-
         let negotiation = new Negotiation(
-            date,
-            parseInt(this._inputQuantity.value),
-            parseFloat(this._inputValue.value)
-        );
+			date,
+			parseInt(this._inputQuantity.value),
+			parseFloat(this._inputValue.value)
+		);
 
-        console.log(negotiation);
-    }
+        let diaMesAno = DataConverter.toText(negotiation.date);
+		console.log(diaMesAno, date);
+	}
 }
---
+++ client/app/ui/converters/DateConverter.js
@@ -0,0 +1,18 @@
+class DataConverter {
+	constructor() {
+		throw new Error("WARN: This class cannot be instantiated!");
+	}
+
+	static toText(str) {
+		return `${str.getDate()}/${str.getMonth() + 1}/${str.getFullYear()}`;
+	}
+
+	static toDate(str) {
+		if (!str.match(/^\d{4}-\d{2}-\d{2}$/)) {
+			throw new Error("WARN: This date must be yyyy-mm-dd");
+		}
+		return new Date(
+			...str.split("-").map((item, indice) => item - (indice % 2))
+		);
+	}
+}
